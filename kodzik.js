afe11e82c6869abfdcb729d5aff01c08a36e2955
unknown <luckiewiczlukasz@gmail.com>
2023-10-17 22:18:13 +0200

    lab3

--- kodzik.js
+++ kodzik.js
@@ -29,13 +29,17 @@ function clear(form){
 }
 
 function changeBackground(number){
+	if(number === null){
+		number = "01"
+	}
 	document.body.style.backgroundImage ='url(obrazki/tla/'+number+".jpg)";
+	localStorage.setItem('background', number);
 }
 
 function gettheDate(){
 	Todays = new Date();
 	TheDate = "" + (Todays.getMonth()+1) +" / "+ Todays.getDate() + " / " +(Todays.getYear()-100);
-	document.getElementById("data").innerHTML = TheDate;
+	$("#data").html(TheDate);
 }
 
 var timerID = null;
@@ -63,7 +67,69 @@ function showtime(){
 	timeValue += (minutes < 10) ? ":0"+minutes : ":" + minutes
 	timeValue += (seconds < 10) ? ":0"+seconds : ":" + seconds
 	timeValue += (hours >= 12) ? "PM" : "AM"
-	document.getElementById("zegarek").innerHTML = timeValue;
+	$("#zegarek").html(timeValue);
 	timerID = setTimeout("showtime()",1000);
 	timerRunning = true;
 }
+
+function loadsite(){
+	$.get('template.html', function(data){
+		if (!$('.tresc').html()) {
+            var content = $('body').html();
+			$('body').html(data);
+            $('.tresc').html(content);
+            startclock();
+        }
+	});
+	$(window).scroll(function(){
+		$("#cover").css({"margin-top": ($(window).scrollTop()) + "px"});
+	});
+	changeBackground(localStorage.getItem('background'));
+	startclock();
+}
+
+var $on = false;
+document.addEventListener("click", function (event) {
+	if(event.target.tagName === "IMG") {
+		var $img = $(event.target);
+		if($on === false){
+			$img.attr("id", "picked");
+			$("#picked").animate({
+				width: "75%"
+			},1500);
+			$("#picked").css({
+				"z-index": '10',
+				"position": 'relative'
+			});
+			$("#cover").css('background-color', 'rgba(0, 0, 0, 0.5)');
+			$("#cover").css("z-index", '5');
+			$on = true;
+		}
+		else{
+			$("#picked").animate({
+				width: "50%"
+			},1500);
+			$("#picked").css({
+				"z-index": '10',
+				"position": 'static'
+			});
+			$("#cover").css('background-color', 'rgba(0, 0, 0, 0)');
+			$("#cover").css("z-index", '-1');
+			$on = false;
+			$("#picked").removeAttr("id");
+		}
+	}
+	else{
+		$("#picked").animate({
+			width: "50%"
+		},1500);
+		$("#picked").css({
+			"z-index": '10',
+			"position": 'static'
+		});
+		$("#cover").css('background-color', 'rgba(0, 0, 0, 0)');
+		$("#cover").css("z-index", '-1');
+		$on = false;
+		$("#picked").removeAttr("id");
+	};
+})
